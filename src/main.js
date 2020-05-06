65ba5854e40628ac2f32cd305d75f68a8634092a
yym04 <yuanyanmeicoco@163.com>
2020-05-06 16:39:15 +0800

    city-picker

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store/index'
 import filters from './filters/index'
 import wechat from '@/common/wechat/index'
+import { Picker } from 'vant'// 引入vant
 Vue.use(wechat);
+Vue.use(Picker)
 // 注入全局过滤器
 Object.keys(filters).forEach(item => {
     Vue.filter(item, filters[item])
--- src/views/city.vue
+++ src/views/city.vue
@@ -6,10 +6,16 @@
             <p class="form-title-sub">选择城市地址提交</p>
             <form class="m-form">
                 <div class="form-item">
-                    <input type="phone" v-model="phone"  class="form-input" placeholder="请选择城市" >
+                    <button class="choose-city" @click="showPicker = !showPicker"></button>
+                    <input type="text"  v-model="city"  class="form-input" placeholder="请选择城市" >
                 </div>
                 <SubmitBtn text="提交报名" toLink="/"/>
             </form>
+        </div>
+        <div class="city-picker">
+            <transition name="pull-up" >
+                <van-picker v-if="showPicker"  show-toolbar title="请选择城市" :columns="columns" />
+            </transition>
         </div>
     </div>
 </template>
@@ -18,7 +24,38 @@ import PageHeader from '../components/PageHeader'
 import SubmitBtn from '../components/SubmitBtn'
 export default {
     data() {
-        return {}
+        return {
+            city: '',
+            showPicker: false,
+            columns: [
+                {
+                    text: '浙江',
+                    children: [
+                        {
+                            text: '杭州',
+                            children: [{ text: '西湖区' }, { text: '余杭区' }]
+                        },
+                        {
+                            text: '温州',
+                            children: [{ text: '鹿城区' }, { text: '瓯海区' }]
+                        }
+                    ]
+                },
+                {
+                    text: '福建',
+                    children: [
+                        {
+                            text: '福州',
+                            children: [{ text: '鼓楼区' }, { text: '台江区' }]
+                        },
+                        {
+                            text: '厦门',
+                            children: [{ text: '思明区' }, { text: '海沧区' }]
+                        }
+                    ]
+                }
+            ]
+        }
     },
     components: {
         PageHeader, SubmitBtn
@@ -26,5 +63,29 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-
+    .choose-city{
+        width: 24px;
+        height: 24px;
+        position: absolute;
+        right: 27px;
+        border: none;
+        background-color: #FFFFFF;
+        @include bg_image("../assets/pictures/city_right")
+    }
+    .van-picker{
+        bottom: -120px;
+    }
+    /* 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    .pull-up-enter-active {
+        transition: all .3s ease;
+    }
+    .pull-up-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .pull-up-enter, .pull-up-leave-to
+        /* .slide-fade-leave-active for below version 2.1.8 */ {
+        transform: translateY(10px);
+        opacity: 0;
+    }
 </style>
